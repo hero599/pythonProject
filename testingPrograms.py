1074d135482fef61dd8cb83fd2a8733f11bcfb6b
hero599 <79750962+hero599@users.noreply.github.com>
2021-05-29 14:23:26 +0530

    First commit

---
+++ testingPrograms.py
@@ -0,0 +1,10 @@
+print('Hello Harpreet')
+num1: float = 1.5
+num2: float = 2.0
+sumNum: float = num1 + num2
+print('The sum of {0} and {1} is {2}' . format(num1, num2, sumNum))
+
+num1 = input('Enter num1: ')
+num2 = input('Enter num2: ')
+sumNum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}' . format(num1, num2, sumNum))
